3f9250c2bc654a99b5d8e090308d4ad313c6a9df
Shaikbabarsk <131939050+Shaikbabarsk@users.noreply.github.com>
2023-08-11 12:39:12 +0530

    Add files via upload

--- heap.c
+++ heap.c
@@ -2,7 +2,6 @@
   
   #include <stdio.h>
   
-
   void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
@@ -21,36 +20,32 @@
     if (right < n && arr[right] > arr[largest])
       largest = right;
   
-   
     if (largest != i) {
       swap(&arr[i], &arr[largest]);
       heapify(arr, n, largest);
     }
   }
   
-  
+
   void heapSort(int arr[], int n) {
- 
+   
     for (int i = n / 2 - 1; i >= 0; i--)
       heapify(arr, n, i);
-  
-  
+
     for (int i = n - 1; i >= 0; i--) {
       swap(&arr[0], &arr[i]);
-  
-          heapify(arr, i, 0);
+ 
+      heapify(arr, i, 0);
     }
   }
   
- 
+  
   void printArray(int arr[], int n) {
     for (int i = 0; i < n; ++i)
       printf("%d ", arr[i]);
     printf("\n");
   }
   
-
- 
   int main() {
     int arr[] = {1, 12, 9, 5, 6, 10};
     int n = sizeof(arr) / sizeof(arr[0]);
