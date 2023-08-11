5b832e085f7d608584553497f0188ad9a8e09628
Shaikbabarsk <131939050+Shaikbabarsk@users.noreply.github.com>
2023-08-11 12:50:27 +0530

    BFSgraph.c

--- BFSgraph.c
+++ BFSgraph.c
@@ -3,17 +3,10 @@
 #include <stdlib.h>
 
 #define MAX_VERTICES 50
-
-// This struct represents a directed graph using
-// adjacency list representation
 typedef struct Graph_t {
-
-	// No. of vertices
 	int V;
 	bool adj[MAX_VERTICES][MAX_VERTICES];
 } Graph;
-
-// Constructor
 Graph* Graph_create(int V)
 {
 	Graph* g = malloc(sizeof(Graph));
@@ -27,44 +20,24 @@ Graph* Graph_create(int V)
 
 	return g;
 }
-
-// Destructor
 void Graph_destroy(Graph* g) { free(g); }
-
-// Function to add an edge to graph
 void Graph_addEdge(Graph* g, int v, int w)
 {
-	// Add w to v’s list.
 	g->adj[v][w] = true;
 }
-
-// Prints BFS traversal from a given source s
-void Graph_BFS(Graph* g, int s)
 {
-	// Mark all the vertices as not visited
 	bool visited[MAX_VERTICES];
 	for (int i = 0; i < g->V; i++) {
 		visited[i] = false;
 	}
-
-	// Create a queue for BFS
 	int queue[MAX_VERTICES];
 	int front = 0, rear = 0;
-
-	// Mark the current node as visited and enqueue it
 	visited[s] = true;
 	queue[rear++] = s;
 
 	while (front != rear) {
-
-		// Dequeue a vertex from queue and print it
 		s = queue[front++];
 		printf("%d ", s);
-
-		// Get all adjacent vertices of the dequeued
-		// vertex s.
-		// If an adjacent has not been visited,
-		// then mark it visited and enqueue it
 		for (int adjacent = 0; adjacent < g->V;
 			adjacent++) {
 			if (g->adj[s][adjacent] && !visited[adjacent]) {
@@ -74,11 +47,8 @@ void Graph_BFS(Graph* g, int s)
 		}
 	}
 }
-
-// Driver code
 int main()
 {
-	// Create a graph
 	Graph* g = Graph_create(4);
 	Graph_addEdge(g, 0, 1);
 	Graph_addEdge(g, 0, 2);
